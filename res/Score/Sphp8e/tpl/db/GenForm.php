4ab27025465d0b88a33646f4db78f5baa6a95a2a
sarta <sarta@192.168.56.1>
2024-03-14 15:18:54 -0400

    Fix PHP stream bug

---
+++ res/Score/Sphp8e/tpl/db/GenForm.php
@@ -0,0 +1,9 @@
+<div class="paghead" id="frmerrdet"><?php print traceError(true); ?></div>
+<div class="paghead" id="frmmsgdet"><?php print traceMsg(true); ?></div>
+<div class="paghead" id="frmformhead"><?php print $formHead; ?></div>
+<form id="form2" runat="server" action="<?php echo getThisURL('',true); ?>">
+<div id="genForm" path="libpath/comp/data/DTable.php" dtable="usert" runat="server" funsetForm="form2" funsetDontUseFormat=""
+ oncreate="<?php global $formFields; eval($formFields); ?>"></div>
+<div align="center" class="pagbar"><input type="submit" value="Submit" /></div>
+</form>
+<div class="pagfooter">&nbsp;</div>
